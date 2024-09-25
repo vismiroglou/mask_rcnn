450ecd9abcb4f1f6a4433458e85c7cc36a72a1f5
vismiroglou <vasilikii@create.aau.dk>
2024-09-25 13:39:54 +0200

    Initial commit

---
+++ visualize.py
@@ -0,0 +1,124 @@
+"""
+Mask R-CNN
+Display and Visualization Functions.
+
+Adapted from:
+https://github.com/matterport/Mask_RCNN
+"""
+
+import random
+import colorsys
+
+import numpy as np
+from skimage.measure import find_contours
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+############################################################
+#  Visualization
+############################################################
+
+def display_instances(image, 
+                      boxes, 
+                      masks, 
+                      class_ids, 
+                      coco,
+                      scores,
+                      threshold=0.5, 
+                      show_mask=True, 
+                      show_bbox=True,
+                      ):
+    """
+    boxes: [num_instance, (x1, y1, x2, y2)] in image coordinates.
+    masks: [num_instances, height, width]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[0] == class_ids.shape[0]
+
+    # If no axis is passed, create one and automatically call show()
+    
+    _, ax = plt.subplots(1, figsize=(16,16))
+
+    # Generate random colors
+    colors = random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        if scores[i] > threshold:
+            color = colors[i]
+
+            # Bounding box
+            x1, y1, x2, y2 = boxes[i]
+            if show_bbox:
+                p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                    alpha=0.7, linestyle="dashed",
+                                    edgecolor=color, facecolor='none')
+                ax.add_patch(p)
+
+            # Label
+            class_id = class_ids[i].item()
+            score = scores[i] if scores is not None else None
+            label = coco.loadCats(class_id)[0]['name']
+            caption = "{} {:.3f}".format(label, score) if score else label
+            ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+            
+
+            # Mask
+            mask = masks[i]
+            if show_mask:
+                masked_image = apply_mask(masked_image, mask, color)
+
+            # Mask Polygon
+            # Pad to ensure proper polygons for masks that touch image edges.
+            padded_mask = np.zeros(
+                (mask.shape[1] + 2, mask.shape[2] + 2), dtype=np.uint8)
+            padded_mask[1:-1, 1:-1] = mask.numpy()
+            contours = find_contours(padded_mask, 0.5)
+            for verts in contours:
+                # Subtract the padding and flip (y, x) to (x, y)
+                verts = np.fliplr(verts) - 1
+                p = patches.Polygon(verts, facecolor="none", edgecolor=color)
+                ax.add_patch(p)
+        ax.imshow(masked_image.astype(np.uint8))
+    plt.show()
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """Apply the given mask to the image.
+    """
+    for c in range(3):
+        image[:, :, c] = np.where(mask > 0.5,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
